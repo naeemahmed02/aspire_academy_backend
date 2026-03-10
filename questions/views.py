9afa400afc7239842ecb3ea5c65e10e256433373
naeemahmed02 <naeemahmedbdn2002@gmail.com>
2026-03-10 20:49:24 +0500

    quizzes serializers.py

--- questions/views.py
+++ questions/views.py
@@ -70,6 +70,5 @@ class QuestionViewSet(viewsets.ModelViewSet):
 
     
     # Automatically assign creator
-    
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
