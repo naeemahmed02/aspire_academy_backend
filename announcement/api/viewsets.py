cac04eed33daa56ff7f388924b39bd73c8bc95d7
naeemahmed02 <naeemahmedbdn2002@gmail.com>
2026-05-01 22:27:21 +0500

    aad announcmenr

--- announcement/api/viewsets.py
+++ announcement/api/viewsets.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from .models import Announcement
+from announcement.models import Announcement
 from .serializers import AnnouncementSerializer
 from rest_framework.permissions import IsAdminUser
 
