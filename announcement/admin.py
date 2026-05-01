a9dce4fae78ac0f5760ad5a5a749a282ecd151bb
naeemahmed02 <naeemahmedbdn2002@gmail.com>
2026-05-01 22:32:49 +0500

    aad announcmenr

--- announcement/admin.py
+++ announcement/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
+from .models import Announcement
 
-# Register your models here.
+
+@admin.register(Announcement)
+class AnnouncementAdmin(ModelAdmin):
+    list_display = (
+        "title",
+        "is_active",
+        "is_urgent",
+        "start_date",
+        "end_date",
+        "created_at",
+    )
+    list_filter = ("is_active", "is_urgent", "created_at")
+    search_fields = ("title", "message")
+    ordering = ("-created_at",)
+
+    fieldsets = (
+        ("Basic Information", {
+            "fields": ("title", "message")
+        }),
+        ("Status", {
+            "fields": ("is_active", "is_urgent")
+        }),
+        ("Schedule", {
+            "fields": ("start_date", "end_date")
+        }),
+        ("Timestamps", {
+            "fields": ("created_at", "updated_at"),
+        }),
+    )
+
+    readonly_fields = ("created_at", "updated_at")
