41d55352206e03992aa93706f9214a8a55896c08
naeemahmed02 <naeemahmedbdn2002@gmail.com>
2026-05-02 11:54:41 +0500

    Student Account configurations

--- accounts/models.py
+++ accounts/models.py
@@ -124,7 +124,7 @@ class Account(AbstractBaseUser, PermissionsMixin):
     date_joined = models.DateTimeField(default=timezone.now)
     last_login = models.DateTimeField(blank=True, null=True)
 
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
 
